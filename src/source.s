b3a69af7076832ae31de89b8ee9be33ac6f5b70e
Georg Ziegler <georgjziegler@gmail.com>
2019-12-26 11:24:13 +0100

    Refactor code

--- src/source.s
+++ src/source.s
@@ -8,23 +8,23 @@ ADKCON		EQU		$dff09e
 INTENA		EQU		$dff09a
 INTREQ		EQU		$dff09c
 
-BPLCON0         EQU             $dff100
-BPLCON1         EQU             $dff102
-BPL1MOD         EQU             $dff108
-BPL2MOD         EQU             $dff10a
-DIWSTRT         EQU             $dff08e
-DIWSTOP         EQU             $dff090
-DDFSTRT         EQU             $dff092
-DDFSTOP         EQU             $dff094
-VPOSR           EQU             $dff004
-COP1LCH         EQU             $dff080
+BPLCON0     EQU     $dff100
+BPLCON1     EQU     $dff102
+BPL1MOD     EQU     $dff108
+BPL2MOD     EQU     $dff10a
+DIWSTRT     EQU     $dff08e
+DIWSTOP     EQU     $dff090
+DDFSTRT     EQU     $dff092
+DDFSTOP     EQU     $dff094
+VPOSR       EQU     $dff004
+COP1LCH     EQU     $dff080
 
-CIAAPRA         EQU             $bfe001
+CIAAPRA     EQU     $bfe001
 
 
 init:
     ; store data in hardwareregisters ORed with $8000
-        ;(bit 15 is a write-set bit when values are written back into the system)
+    ; (bit 15 is a write-set bit when values are written back into the system)
     move.w  DMACONR,d0
     or.w #$8000,d0
     move.w d0,olddmareq
@@ -54,8 +54,8 @@ init:
     move.l	$4,a6
     jsr -132(a6)	; Forbid
 
-        ; setup displayhardware to show a 320x200px 3 bitplanes playfield
-        ; with zero horizontal scroll and zero modulos
+    ; setup displayhardware to show a 320x200px 3 bitplanes playfield
+    ; with zero horizontal scroll and zero modulos
     move.w	#$3200,BPLCON0			; three bitplanes
     move.w	#$0000,BPLCON1			; horizontal scroll 0
     move.w	#$0050,BPL1MOD			; odd modulo
@@ -70,10 +70,10 @@ init:
     move.w 	#%0011111111111111,INTENA	; IRQ set OFF
 
 mainloop:
-        move.l frame,d1
+    move.l frame,d1
     move.l #copper,a6
-        addq.l #1,d1
-        move.l d1,frame
+    addq.l #1,d1
+    move.l d1,frame
 
     ; bitplane 0
     move.l #bitplanes,d0
@@ -109,31 +109,31 @@ mainloop:
     move.l #$018c0000,(a6)+	; color 6
     move.l #$018e0cef,(a6)+	; color 7
 
-        move.l #32,d0 ; Number of iterations
-        move.l #$07,d1 ; Current row wait
-        move.l #sin32_15,a0 ; Sine base
-        move.l frame,d2 ; Current sine
-        scrollrows:
-          ; Wait for correct offset row
-          move.w d1,(a6)+
-          move.w #$fffe,(a6)+
-          ; Fetch sine from table
-          move.l d2,d3
-          and.l #$1f,d3
-          move.b (a0,d3),d4
-          ; Transform sine to horizontal offset value
-          move.l d4,d5
-          lsl.l #4,d4
-          add.l d4,d5
-          ; Add horizontal offset to copperlist
-          move.w #$0102,(a6)+
-          move.w d5,(a6)+
-          ; Proceed to next row that we want to offset
-          add.l #$500,d1
-          ; Move to next sine position for next offset row
-          addq.w #1,d2
-          subq.w #1,d0
-          bne scrollrows
+    move.l #32,d0 ; Number of iterations
+    move.l #$07,d1 ; Current row wait
+    move.l #sin32_15,a0 ; Sine base
+    move.l frame,d2 ; Current sine
+scrollrows:
+    ; Wait for correct offset row
+    move.w d1,(a6)+
+    move.w #$fffe,(a6)+
+    ; Fetch sine from table
+    move.l d2,d3
+    and.l #$1f,d3
+    move.b (a0,d3),d4
+    ; Transform sine to horizontal offset value
+    move.l d4,d5
+    lsl.l #4,d4
+    add.l d4,d5
+    ; Add horizontal offset to copperlist
+    move.w #$0102,(a6)+
+    move.w d5,(a6)+
+    ; Proceed to next row that we want to offset
+    add.l #$500,d1
+    ; Move to next sine position for next offset row
+    addq.w #1,d2
+    subq.w #1,d0
+    bne scrollrows
 
     ; end of copperlist
     move.l #$fffffffe,(a6)+
@@ -144,7 +144,7 @@ mainloop:
     btst.b #7,CIAAPRA
     beq exit
 
-; Wait for vertical blanking before taking the copper list into use
+    ; Wait for vertical blanking before taking the copper list into use
 waitVB:
     move.l VPOSR,d0
     and.l #$1ff00,d0
@@ -157,7 +157,7 @@ waitVB:
     bra mainloop
 
 exit:
-; exit gracefully - reverse everything done in init
+    ; exit gracefully - reverse everything done in init
     move.w #$7fff,DMACON
     move.w	olddmareq,DMACON
     move.w #$7fff,INTENA
@@ -190,7 +190,7 @@ exit:
 oldview:	dc.l 0
 oldcopper:	dc.l 0
 gfxbase:	dc.l 0
-frame:          dc.l 0
+frame:      dc.l 0
 
 ; storage for 16-bit data
     CNOP 0,4
@@ -210,11 +210,11 @@ gfxname: dc.b 'graphics.library',0
 
     CNOP 0,4
 bitplanes:
-  incbin "masters3.raw"
-  blk.b 320/8*3*(200-160),0
+    incbin "masters3.raw"
+    blk.b 320/8*3*(200-160),0
 
 ; datalists aligned to 32-bit
     CNOP 0,4
 copper:
-  dc.l $ffffffe
-  blk.l 1023,0
+    dc.l $ffffffe
+    blk.l 1023,0
